9686433068fe15ba8fe002fd823ed90d269385fe
Ithonga <ithongadev@gmail.com>
2025-03-25 13:23:10 +0300

    updated the projects page

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,30 @@ import Project from "./pages/Projects";
 import Contact from "./pages/Contact";
 import Services from "./pages/Services";
 import Footer from "./Components/Footer";
-import { ScrollTop } from 'primereact/scrolltop';
-
+import { ScrollTop } from "primereact/scrolltop";
+import FadeContent from "./Components/Animation_Motion/CountUp";
 
 function App() {
   return (
     <>
-    <Header />
-    <Sidebar/>
-    <ScrollTop className="w-[50px] h-[50px] bg-red-500 border-[1px] border-white rounded-full text-white"/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/projects" element={<Project />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/services" element={<Services />} />
-      </Routes>
-      <Footer/>
+      <Header />
+      <Sidebar />
+      <FadeContent
+        blur={true}
+        duration={1000}
+        easing="ease-out"
+        initialOpacity={0}
+      >
+        <ScrollTop className="w-[50px] h-[50px] bg-red-500 border-[1px] border-white rounded-full text-white" />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Project />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/services" element={<Services />} />
+        </Routes>
+      </FadeContent>
+      <Footer />
     </>
   );
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,17 +1,18 @@
-import React, { useEffect } from 'react'
-import ErrorPage from '../pages/ErrorPage'
+import React, { useEffect } from "react";
+import ErrorPage from "../pages/ErrorPage";
+import Stack from "../Components/Animation_Motion/ProjectStack";
 function Projects() {
-
   // Reset scroll position to top when navigating to a new route
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   return (
-    <div className=''>
-      <ErrorPage />
+    <div className="flex flex-col  justify-center pt-[120px] ml-[90px]">
+      <h1 className="text-[24px] md:text-[40px] font-bold">Side Projects</h1>
+      <p className="text-[15px] md:text-[20px] ">Whenever I get time, I build side projects. Here you can navigate to different websites, apps, and tools I built.</p>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
