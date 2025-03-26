38c71b3e573051e2212c8e896fa66f24b2c16e68
Ithonga <ithongadev@gmail.com>
2025-03-26 14:09:30 +0300

    updated the projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,47 +3,28 @@ import { motion } from "framer-motion";
 import CountUp from "../Components/Animation_Motion/CountUp";
 
 function Projects() {
+  const projectData = [
+    {
+      title: "Crystal Digital",
+      description:
+        "I have built various websites and web applications using HTML, CSS, JavaScript, and React.",
+      image: "/crystal-digital.png",
+      link: "https://crystaldigital.framer.ai",
+    },
+    {
+      title: "JM Meat Supply",
+      description:
+        "I have built mobile applications using React Native and Flutter.",
+      image: "/jm-meat-supply.png",
+      link: "https://jmmeatsupply.com/",
+    },
+  ];
   // Reset scroll position to top when navigating to a new route
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   return (
-    // <div className="flex flex-col  justify-center pt-[120px] ml-[50px] md:ml-[90px] ">
-    //   <div>
-    //     <h1 className="text-[24px] md:text-[40px] font-bold">Side Projects</h1>
-    //     <p className="text-[15px] md:text-[20px] ">
-    //       Whenever I get time, I build side projects. Here you can navigate to
-    //       different websites, apps, and tools I built.
-    //     </p>
-    //   </div>
-
-    //   <div className="flex-col p-4  md:p-[60px] flex md:flex-row md:gap-10 items-center justify-between text-left">
-    //     <img
-    //       src="/web-development.jpg"
-    //       alt=""
-    //       className="w-full  md:w-[400px] md:h-fit border-[1px] p-5 hover:scale-105 transition-all ease-in-out"
-    //     />
-    //     <div className="flex flex-col">
-    //       <h2 className="text-center md:text-left text-[24px] md:text-[40px] font-bold">
-    //         Crystal Digital{" "}
-    //       </h2>
-    //       <h2 className=" text-[15px] w-full md:w-full text-center md:text-[20px] text-gray-500 font-medium mb-4 md:text-left">
-    //         Crystal Digital is a creative design shop specializing in
-    //         high-quality graphic design, printing, and branding solutions. From
-    //         custom logos to professional business cards and large-scale banners,
-    //         we bring ideas to life with precision and creativity.{" "}
-    //       </h2>
-
-    //       <a
-    //         href=""
-    //         className="mt-2 text-[15px] font-light hover:underline bg-blue-500 p-3 rounded-lg text-white"
-    //       >
-    //         View Demo
-    //       </a>
-    //       </div>
-    //   </div>
-    // </div>
     <div className="pt-[90px] pl-[50px] md:pl-[90px]">
       <div
         className=" relative w-full h-[400px] flex items-center justify-center bg-cover bg-center text-white"
@@ -135,6 +116,28 @@ function Projects() {
             </div>
           </div>
         </div>
+      </div>
+      <div className="flex-row grid md:grid-cols-3 p-3 md:p-[42px] gap-8">
+        {projectData.map((project, index) => (
+          <div
+            key={index}
+            className="flex flex-col gap- w-full items-center shadow-lg overflow-hidden"
+          >
+            <img
+              src={project.image}
+              alt=""
+              className="w-full h-[300px] md:h-[400px] object-cover transition-all duration-500 ease-in-out object-top hover:object-[1%_100%]"
+            />
+
+            <div className="p-4 hover:bg-red-500 transition-all ease-in-out hover:text-white">
+              <h2>{project.title}</h2>
+              <p>{project.description}</p>
+              <a href={project.link} target="_black" className="text-yellow-400">
+                VIEW DEMO
+              </a>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
