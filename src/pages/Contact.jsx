7743f7214fb77c14ff2f441b0fe163a0edb5845d
Ithonga <ithongadev@gmail.com>
2025-03-27 14:47:18 +0300

    updated the contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { motion } from "framer-motion";
+import { IoCall, IoMailOpen } from "react-icons/io5";
 
 function Contact() {
   // Reset scroll position to top when navigating to a new route
@@ -29,6 +30,20 @@ function Contact() {
             Get in touch with me.
           </h1>
         </motion.div>
+      </div>
+      <div className="flex gap-8 w-full items-center justify-center flex-col md:flex-row py-10">
+        <div className="w-[300px] h-[300px] flex flex-col gap-3 items-center justify-center shadow-lg p-5 rounded-lg bg-slate-100">
+          <IoCall className="text-[50px]" />
+          <h2 className="font-bold text-[20px]">Call</h2>
+          <p>+254-741-994-264</p>
+        </div>
+        <div className="w-[300px] h-[300px] flex flex-col gap-3 items-center justify-center shadow-lg p-5 rounded-lg bg-slate-100">
+          <IoMailOpen className="text-[50px]" />
+          <h2 className="font-bold text-[20px]">Email</h2>
+          <p>ithongadev@gmail.com</p>
+          <p>ndwarurony004@gmail.com</p>
+        </div>
+        <div></div>
       </div>
     </div>
   );
