f88954cfe10375915106aa2a37c28d857b48755b
Ithonga <ithongadev@gmail.com>
2025-04-02 17:49:45 +0300

    updated the contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 import { IoCall, IoMailOpen } from "react-icons/io5";
+import Contact from "../Components/Contact";
 
-function Contact() {
+function ContactPage() {
   // Reset scroll position to top when navigating to a new route
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -11,7 +12,7 @@ function Contact() {
     <div className="pt-[90px] pl-[50px] md:pl-[90px]">
       <div
         className=" relative w-full h-[400px] flex items-center justify-center bg-cover bg-center text-white"
-        style={{ backgroundImage: "url('/contact.webp')" }} // Replace with your image path
+        style={{ backgroundImage: "url('/contact.webp')" }}
       >
         {/* Overlay */}
         <div className="absolute inset-0 bg-black bg-opacity-50"></div>
@@ -31,22 +32,24 @@ function Contact() {
           </h1>
         </motion.div>
       </div>
-      <div className="flex gap-8 w-full items-center justify-center flex-col md:flex-row py-10">
-        <div className="w-[300px] h-[300px] flex flex-col gap-3 items-center justify-center shadow-lg p-5 rounded-lg bg-slate-100">
-          <IoCall className="text-[50px]" />
-          <h2 className="font-bold text-[20px]">Call</h2>
-          <p>+254-741-994-264</p>
+      <div className="flex md:flex-row flex-col justify-center py-10 px-3 gap-8 md:items-start">
+        <div className="flex flex-col gap-8 items-center justify-center">
+          <div className="w-full h-[300px] flex flex-col gap-3 items-center justify-center shadow-lg p-5 rounded-lg bg-slate-100">
+            <IoCall className="text-[50px]" />
+            <h2 className="font-bold text-[20px]">Call</h2>
+            <p>+254-741-994-264</p>
+          </div>
+          <div className="w-full h-[300px] flex flex-col gap-3 items-center justify-center shadow-lg p-5 rounded-lg bg-slate-100">
+            <IoMailOpen className="text-[50px]" />
+            <h2 className="font-bold text-[20px]">Email</h2>
+            <p>ithongadev@gmail.com</p>
+            <p>ndwarurony004@gmail.com</p>
+          </div>
         </div>
-        <div className="w-[300px] h-[300px] flex flex-col gap-3 items-center justify-center shadow-lg p-5 rounded-lg bg-slate-100">
-          <IoMailOpen className="text-[50px]" />
-          <h2 className="font-bold text-[20px]">Email</h2>
-          <p>ithongadev@gmail.com</p>
-          <p>ndwarurony004@gmail.com</p>
-        </div>
-        <div></div>
+        <Contact />
       </div>
     </div>
   );
 }
 
-export default Contact;
+export default ContactPage;
