e29dd994709326b1eed6349ffe581c132d37af7a
Ithonga <ithongadev@gmail.com>
2025-04-02 17:49:16 +0300

    added images for the skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,53 +1,57 @@
 import React, { useEffect } from "react";
 import { motion } from "framer-motion";
-import Folder from "../Components/Animation_Motion/Folder";
+
 function Skills() {
-  const technicalskills = [
+  const technicalSkills = [
     {
       name: "React",
-      image : "/"
+      image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
     },
     {
       name: "React Native",
-      image : "/"
+      image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
     },
     {
-      name: "Html",
-      image : "/"
+      name: "HTML",
+      image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
     },
     {
-      name: "Css",
-      image : "/"
+      name: "CSS",
+      image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
     },
     {
       name: "Corel-Draw",
-      image : "/"
+      image: "https://seeklogo.com/images/C/coreldraw-x7-logo-747B0DC253-seeklogo.com.png",
     },
     {
       name: "Firebase",
-      image : "/"
+      image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
     },
     {
-      name: "Javascript",
-      image : "/"
+      name: "JavaScript",
+      image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
     },
     {
-      name: "Typescript",
-      image : "/"
+      name: "TypeScript",
+      image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
     },
   ];
+
   // Reset scroll position to top when navigating to a new route
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   return (
-    <div className="pl-3">
+    <div className="p-3">
       <h2 className="text-[24px] font-bold py-4">My Technical Level</h2>
-      <motion.div className="grid-cols-2 grid md:grid-cols-4 gap-4">
-        {technicalskills.map((skill, index) => (
-          <div key={index} className="bg-gray-100 hover:bg-[#00B0FF] gap-5 p-4 py-10 shadow-lg rounded-3xl flex flex-col items-center justify-center">
-           <img src={skill.image} className="w-[50px] h-[50px]"/>
+      <motion.div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+        {technicalSkills.map((skill, index) => (
+          <div
+            key={index}
+            className="bg-gray-100 gap-5 p-4 py-10 shadow-lg rounded-3xl flex flex-col items-center justify-center"
+          >
+            <img src={skill.image} alt={skill.name} className="w-[50px] h-[50px]" />
             <h1 className="font-medium">{skill.name}</h1>
           </div>
         ))}
