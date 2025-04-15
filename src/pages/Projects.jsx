114933fb81842c7ff913a4f3450e227c6602ffdf
Ithonga <ithongadev@gmail.com>
2025-04-15 19:10:04 +0300

    update

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -151,7 +151,7 @@ function Projects() {
         <h2 className="text-[24px] md:text-[45px] font-bold p-3 md:p-[42px]">
           Featured and Ongoing Projects
         </h2>
-        <div className="flex-row grid md:grid-cols-5 p-3 md:p-[42px] gap-8">
+        <div className="flex-row grid sm:grid-cols-3 md:grid-cols-5 p-3 md:p-[42px] gap-8">
           {projectData.map((project, index) => (
             <div
               key={index}
@@ -163,7 +163,7 @@ function Projects() {
                 className="w-full h-1/2 mdh-[400px] object-cover transition-all duration-500 ease-in-out object-top hover:object-[1%_100%]"
               />
 
-              <div className="p-4 transition-all ease-in-out flex flex-col gap-3">
+              <div className="w-full  p-2 transition-all ease-in-out flex flex-col gap-3">
                 <h2 className="text-[20px] font-bold">{project.title}</h2>
                 <p className=" text-[16px]">
                   {project.description.slice(0, 80)}...
