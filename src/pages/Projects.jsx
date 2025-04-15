723b2efc52bb6ce81a7b42d365d82b9e6d0fc2a2
Ithonga <ithongadev@gmail.com>
2025-04-15 19:01:31 +0300

    redesigned the projects card

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,7 +6,7 @@ import { Helmet } from "react-helmet";
 function Projects() {
   const projectData = [
     {
-      title: "Crystaldigital.co.ke",
+      title: "crystaldigital.co.ke",
       description:
         "Welcome to a world where visual storytelling means innovation, and creativity knows no bounds. From sleek logos that speak volumes to immersive web experiences.",
       image: "/Crystal-Digital1.png",
@@ -14,19 +14,19 @@ function Projects() {
     },
     {
       title: "ronnieithonga.com",
-      description: "",
+      description: "I'm a self-taught React Front-End Developer who builds easy-to-use websites and apps.I turn complex ideas into simple, user-friendly designs with attention to detail.I love solving problems and learning new things.My code is clean,works fast and is accessible to everyone.Let's work together to create tools people love and make your brand stand out.",
       image: "/ronnie-ithonga.png",
       link: "https://ronnieithonga.com",
     },
     {
-      title: "Crystal Digital",
+      title: "crystaldigital.com",
       description:
         "Welcome to a world where visual storytelling means innovation, and creativity knows no bounds. From sleek logos that speak volumes to immersive web experiences.",
       image: "/crystal-digital.png",
       link: "https://crystaldigital.framer.ai",
     },
     {
-      title: "JM Meat Supply",
+      title: "jmmeatsupply.com",
       description:
         "FreshCuts Meat delivers high-quality, farm-fresh meat straight to your doorstep. Enjoy premium cuts of beef, chicken, lamb, and more with fast and reliable delivery.",
       image: "/jm-meat-supply.png",
@@ -155,12 +155,12 @@ function Projects() {
           {projectData.map((project, index) => (
             <div
               key={index}
-              className="flex flex-col w-full h-[450px] items-center shadow-lg overflow-hidden"
+              className="flex flex-col w-full h-[400px] items-center shadow-lg overflow-hidden"
             >
               <img
                 src={project.image}
                 alt=""
-                className="w-full h-[300px] md:h-[400px] object-cover transition-all duration-500 ease-in-out object-top hover:object-[1%_100%]"
+                className="w-full h-1/2 mdh-[400px] object-cover transition-all duration-500 ease-in-out object-top hover:object-[1%_100%]"
               />
 
               <div className="p-4 transition-all ease-in-out flex flex-col gap-3">
@@ -171,9 +171,9 @@ function Projects() {
                 <a
                   href={project.link}
                   target="_black"
-                  className="text-white p-2 rounded-lg bg-blue-500 self-baseline"
+                  className="text-white p-1 rounded-lg bg-blue-500 self-baseline"
                 >
-                  VIEW DEMO
+                  Preview
                 </a>
               </div>
             </div>
